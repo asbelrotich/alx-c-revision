72d4f59d7aeda3abae559637f642777994c9ed09
asbel-software-engineer <asbelkipkosgei0@gmail.com>
2024-02-09 05:20:39 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Puts function to print "programming is like building
+ * multilingual puzzle" followed by new line
+ * return 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building  multilingual puzzle");
+	return (0);
+}
