dfbc7e3d965e8bcab6870cdeeee52834ae068dcf
asbel-software-engineer <asbelkipkosgei0@gmail.com>
2024-02-11 13:05:36 -0800

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - To print numbers from 0 to 9
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int c;
+
+	for(c = 0; c < 10; c++)
+		printf("%d", c);
+	printf("\n");
+	return (0);
+}
