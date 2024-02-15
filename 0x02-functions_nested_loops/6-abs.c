eb35080d64e50362478d5cf8a68a1c8c4431e964
asbel-software-engineer <asbelkipkosgei0@gmail.com>
2024-02-15 14:08:48 -0800

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - To convert to absolute
+ * return asucess
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+	return(a * -1);
+	}
+	else
+	{
+	return(a * 1);
+	}
+	return (0);
+}
