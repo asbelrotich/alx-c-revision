5ff202a4e1bc0ff3616356acd39cc4fa909a1162
asbel-software-engineer <asbelkipkosgei0@gmail.com>
2024-02-18 06:44:41 -0800

    Number times ten

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/** 
+ * more_numbers: To print numbersv 10 times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (b = 0; b < 10; b++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			if (a > 9)
+			{
+				_putchar((a / 10) + '0');
+			}
+			_putchar((a % 10) + '0');
+		}
+		_putchar ('\n');
+	}
+}
