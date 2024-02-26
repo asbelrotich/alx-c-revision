ed12a056308e5cb28916342de703f6bc10c9187c
asbel-software-engineer <asbelkipkosgei0@gmail.com>
2024-02-26 07:59:12 -0800

    Puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - It prints digitd in a string
+ * @*str: The pointer value
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
